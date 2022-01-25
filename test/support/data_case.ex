261ccf46e09ad7555bf889fd228538e65a7b7f72
wagner <wagncarv@gmail.com>
2022-01-25 20:35:38 -0300

    config credo

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -15,6 +15,7 @@ defmodule BlogNew.DataCase do
   """
 
   use ExUnit.CaseTemplate
+  Ecto.Adapters.SQL.Sandbox
 
   using do
     quote do
@@ -28,8 +29,8 @@ defmodule BlogNew.DataCase do
   end
 
   setup tags do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(BlogNew.Repo, shared: not tags[:async])
-    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+    pid = Sandbox.start_owner!(BlogNew.Repo, shared: not tags[:async])
+    on_exit(fn -> Sandbox.stop_owner(pid) end)
     :ok
   end
 
