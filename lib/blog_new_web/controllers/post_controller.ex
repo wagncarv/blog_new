e4c6f8892ae4c6d004731caf87048b6f2b556839
wagner <wagncarv@gmail.com>
2022-02-12 18:09:58 -0300

    enviando form para banco de dados

--- lib/blog_new_web/controllers/post_controller.ex
+++ lib/blog_new_web/controllers/post_controller.ex
@@ -18,7 +18,18 @@ defmodule BlogNewWeb.PostController do
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, _params) do
-    # render(conn, "new.html")
+  def create(conn, %{"post" => post}) do
+    post = Post.changeset(%Post{}, post)
+    |> BlogNew.Repo.insert()
+    IO.inspect(post, label: "POST: ")
+
+    case post do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Post criado com sucesso")
+        |> redirect(to: Routes.post_path(conn, :show, post))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 end
